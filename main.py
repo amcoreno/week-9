35c582fb75b7eeb14f60b845b5de5d70e5dd6a5b
Araceli Coreno <113930596+amcoreno@users.noreply.github.com>
2022-10-20 17:59:17 +0000

    just need to get the try counter

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from random import randint
 # 4 And if the user got the secret number right, they will be informed that they have won,and how many tries that has taken them.
 # 5 If the user has not guessed correctly in their first attempt, they will be asked again to choose another number and so on until they win or until their eight attempts are done.
 
+
 playerName = input("What is your name?")
 print(f"Well, {playerName}, I'm thinking of a number between 1 - 100. Try to guess it in under 8 tries.")
 number = input("What number do you think it is?")
@@ -19,11 +20,19 @@ number = input("What number do you think it is?")
 randomNumber = randint(1, 101)
 
 for i in range(8):
-  if number == randomNumber:
-    print("You guessed it right!")
-elif number > randomNumber:
-    print("The number you guessed it more than the number I picked!")
-elif number <  randomNumber:
-    print("The number you guessed is lesser than the number I picked!")
-else:
+  if int(number) == randomNumber:
+    print(f"You guessed it right in {i + 1} tries")
+  elif int(number) > 100:
     print("It's out of range.")
+    number = input("What number do you think it is?")
+  elif int(number) < 1:
+    print("It's out of range.")
+    number = input("What number do you think it is?")
+  elif int(number) > randomNumber:
+    print("The number you guessed it more than the number I picked!") 
+    number = input("What number do you think it is?")
+  elif int(number) <  randomNumber:
+    print("The number you guessed is lesser than the number I picked!")
+    number = input("What number do you think it is?")
+  else:
+      print("You didnt get it")
